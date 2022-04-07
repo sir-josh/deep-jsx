f2537f143518c08d2c5517ff43da094b06458228
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-07 16:31:54 +0100

    add in app component

--- jsz/src/index.js
+++ jsz/src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component
-
+const App = () => {
+    return <div>Hello World, I'm back again</div>
+}
 
 // Take the react component and show it on the screen
